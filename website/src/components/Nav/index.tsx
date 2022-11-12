34a8d7824995d379a4766c9c94b8b28be268aa13
sj <songjie@jz-sh.cn>
2022-11-12 14:39:55 +0800

    fix: 修改暗主题颜色

--- website/src/components/Nav/index.tsx
+++ website/src/components/Nav/index.tsx
@@ -141,7 +141,7 @@ export default function Nav() {
         />
       </div>
       <div className={[styles.btn, state.layout === 'left' ? null : styles.btnTop].filter(Boolean).join(' ').trim()}>
-        <dark-mode light="Light" dark="Dart"></dark-mode>
+        <dark-mode light="Light" dark="Dart" style={{ color: '#fff', marginRight: 10 }}></dark-mode>
         <select value={i18n.language} onChange={(e) => changeLanguage(e)}>
           <option value="zh-CN">简体中文</option>
           <option value="en-US">English</option>
